55610525a30de0173297b132c47067f15c0287a5
Jeaninne <jeaninnel@hotmail.com>
2020-02-23 21:17:03 -0500

    Set up my initial prompts

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pwdLength = prompt("How long would you like the password to be?");
+var lower = confirm("Would you like the password to include LOWERcase characters?");
+var upper = confirm("Would you like the password to contain UPPERcase characters?");
+var nums = confirm("Would you like the password to contain NUMBERS?");
+var symbols = confirm("Would you like the password to contain SYMBOLS?");
 
 // Write password to the #password input
 function writePassword() {
